fc634ce4b03dc9b73bcb7620e83908cc692c19f6
heung <hoonun@gmail.com>
2026-03-02 14:16:06 +0900

    개발환경,운영환경 분리 및 Docker 설정 추가

--- src/main/java/com/cms/admin/AdminMainController.java
+++ src/main/java/com/cms/admin/AdminMainController.java
@@ -14,17 +14,17 @@ public class AdminMainController {
 
     @GetMapping
     public String main(Model model) {
-        return "/admin/index";
+        return "admin/index";
     }
 
     @GetMapping("/login")
     public String login(Model model) {
-        return "/admin/login";
+        return "admin/login";
     }
 
     @GetMapping("/login-error")
     public String loginError(Model model) {
         model.addAttribute("error", "아이디 또는 비밀번호가 올바르지 않습니다.");
-        return "/admin/login";
+        return "admin/login";
     }
 }
